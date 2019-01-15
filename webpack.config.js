0561c1e9380bd2e6527df1746e5db51b26c1d08a
gcvin <gc.vin@qq.com>
2019-01-15 15:07:39 +0800

    fix: add git hook

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-module.exports = {
+const isprod = process.env.NODE_ENV === 'production'
+const clean = new CleanWebpackPlugin(['./dist', './index.html'])
+
+const config = {
+  output: {
+    filename: '[name].[hash:8].js'
+  },
   module: {
     rules: [
       {
@@ -29,24 +36,37 @@ module.exports = {
         test: /\.(png|jpg)$/,
         use: {
           loader: 'url-loader',
-          options: { limit: 8129 }
+          options: {
+            limit: 8129,
+            name: '[name].[hash:8].[ext]'
+          }
         }
       }
     ]
   },
+  stats: {
+    modules: false,
+    children: false,
+    entrypoints: false
+  },
   devServer: {
+    stats: 'minimal',
     inline: true,
     hot: true
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html"
+      filename: "../index.html"
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
+      filename: "[name].[hash:8].css",
+      chunkFilename: "[id].[hash:8].css"
     }),
     new webpack.HotModuleReplacementPlugin()
   ]
 }
+
+isprod && config.plugins.unshift(clean)
+
+module.exports = config
