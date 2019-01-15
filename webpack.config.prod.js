97a8bba58ce552cce8941ea52f606f6095433dde
gcvin <gc.vin@qq.com>
2019-01-15 15:43:12 +0800

    fix: correct webpack image path

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,9 @@
-const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
-const isprod = process.env.NODE_ENV === 'production'
-const clean = new CleanWebpackPlugin(['./dist', './index.html'])
-
-const config = {
+module.exports = {
+  mode: 'production',
   output: {
     filename: '[name].[hash:8].js'
   },
@@ -38,7 +35,8 @@ const config = {
           loader: 'url-loader',
           options: {
             limit: 8129,
-            name: '[name].[hash:8].[ext]'
+            name: '[name].[hash:8].[ext]',
+            publicPath: './dist'
           }
         }
       }
@@ -49,12 +47,8 @@ const config = {
     children: false,
     entrypoints: false
   },
-  devServer: {
-    stats: 'minimal',
-    inline: true,
-    hot: true
-  },
   plugins: [
+    new CleanWebpackPlugin(['./dist', './index.html']),
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "../index.html"
@@ -62,11 +56,6 @@ const config = {
     new MiniCssExtractPlugin({
       filename: "[name].[hash:8].css",
       chunkFilename: "[id].[hash:8].css"
-    }),
-    new webpack.HotModuleReplacementPlugin()
+    })
   ]
 }
-
-isprod && config.plugins.unshift(clean)
-
-module.exports = config
