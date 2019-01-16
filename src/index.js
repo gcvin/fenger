889f2390ba6a84fefdf77cc21353be02837aad35
gcvin <gc.vin@qq.com>
2019-01-16 11:02:52 +0800

    feat: add barrage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./index.css"
 import moment from 'moment'
+import axios from 'axios'
 
 const root = document.getElementById('missing_her')
 const start = moment('20180710', 'YYYYMMDD')
 const days = moment().diff(start, 'day')
-const daysDom = missing_her.querySelector('.missing_her_days')
+const daysDom = root.querySelector('.missing_her_days')
 
 // 创建个位、十位、百位...数字元素
 const createNumber = () => {
@@ -49,8 +50,42 @@ const timer = setInterval(() => {
   }
 }, 100)
 
-// 文字颜色渐变
-setInterval(() => {
-  const positionX = parseInt(root.style.backgroundPositionX) || 0
-  root.style.backgroundPositionX = positionX + 10 + 'px'
-}, 100)
+// 评论弹幕
+const barrage = (ele, option) => {
+  const div = document.createElement('div')
+  const img = document.createElement('img')
+  const txt = document.createTextNode(option.body)
+
+  const rows = ele.clientHeight / 28
+  const duration = ~~(Math.random() * 5000) + 15000
+  const delay = ~~(Math.random() * 5000)
+  const colors = ['#f00', '#f60', '#0c0', '#699', '#06c', '#909']
+
+  img.src = option.user.avatar_url
+  div.appendChild(img)
+  div.appendChild(txt)
+
+  div.className = 'text'
+  div.style.top = ~~(Math.random() * rows) * 28 + 'px' 
+  div.style.transitionDuration = duration / 1000 + 's'
+  div.style.transitionDelay =  delay / 1000 + 's'
+  div.style.backgroundColor = colors[~~(Math.random() * 6)]
+
+  ele.appendChild(div)
+
+  div.style.left = -div.clientWidth + 'px'
+
+  setTimeout(() => {
+    ele.removeChild(div)
+  }, duration + delay)
+}
+
+const ele = root.querySelector('.missing_her_barrage')
+const url = 'https://api.github.com/repos/gcvin/fenger/issues/3/comments'
+
+// 请求github评论
+axios.get(url).then(rs => {
+  rs.data.forEach(item => {
+    barrage(ele, item)
+  })
+})
